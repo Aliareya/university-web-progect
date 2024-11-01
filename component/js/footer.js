514666c6896dbfee3ec9fab4f30f8bc76c015b84
Ali <aliareya2003@gmail.com>
2024-11-01 15:43:51 +0430

    d

--- component/js/footer.js
+++ component/js/footer.js
@@ -10,28 +10,23 @@ footer_content.innerHTML = `
         <div class="contact-part">
             <span>
                 <img src="./image/phone.png" width="30px" alt="">
-                <h3>Phone Number:</h3>
-                <p>(060) 444 434 44</p>
+                <h3>Phone</h3>
             </span>
             <span>
                 <img src="./image/emergency.png" width="30px" alt="">
                 <h3>Emargency</h3>
-                <p>(060) 444 434 44</p>
             </span>
             <span>
                 <img src="./image/email.png" width="30px" alt="">
                 <h3>Email</h3>
-                <p>salamet_hospital@gmail.com</p>
             </span>
             <span>
                 <img src="./image/facebook.png" width="30px" alt="">
                 <h3>Facebook</h3>
-                <p>https://facebook.com/salamet</p>
             </span>
             <span>
                 <img src="./image/twitter.png" width="30px" alt="">
                 <h3>Twitter</h3>
-                <p>x.com/salamet_hospital</p>
             </span>
         </div>
        </div>
