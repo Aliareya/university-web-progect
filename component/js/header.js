c1568e5d4ebefdabfc18e997668573c0477be250
Ehsanullah946 <Ehsanullahakbari53@gmail.com>
2024-11-09 22:37:02 +0430

    done departement part

--- component/js/header.js
+++ component/js/header.js
@@ -1,11 +1,11 @@
-let man = document.querySelector('header');
+let man = document.querySelector("header");
 
 const screenWidth = window.innerWidth;
 console.log("Screen width is: " + screenWidth + "px");
 
 let man1 = 1;
-let nweheader = document.createElement('div');
-nweheader.className="header-info";
+let nweheader = document.createElement("div");
+nweheader.className = "header-info";
 nweheader.innerHTML = `
          <div class="logo">
            <img src="./image/logo.png" width="70" height="70">
@@ -17,11 +17,12 @@ nweheader.innerHTML = `
            <li><a href="#">Doctors</a></li>
            <li><a href="#">Emergency</a></li>
            <li><a href="#">Pharmacy</a></li>
-           ${screenWidth !== 1024 ?
-            '<li id="last-menu"><a href="#">Schedule an appointment</a></li>'
-            :
-            '<li id="last-menu"><a href="#">Get an appointment</a></li>'
-          }
+           ${
+             screenWidth !== 1024
+               ? '<li id="last-menu"><a href="#">Schedule an appointment</a></li>'
+               : '<li id="last-menu"><a href="#">Get an appointment</a></li>'
+           }
          </div>
 `;
+
 man.appendChild(nweheader);
